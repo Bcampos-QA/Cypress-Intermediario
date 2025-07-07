aaad936920698b1db33cbd7219036d32568c2089
Bruno Campos <brunoguerreiro.campos@hotmail.com>
2025-07-07 11:09:27 -0300

    Testes de login

---
+++ cypress/support/gui_commands.js
@@ -0,0 +1,20 @@
+// Adiciona um comando customizado ao Cypress chamado "login"
+Cypress.Commands.add('login', (
+  user = Cypress.env('user_name'),      // Pega o nome de usuário das variáveis de ambiente
+  password = Cypress.env('user_password') // Pega a senha das variáveis de ambiente
+) => {
+  const login = () => {
+    cy.visit('/users/sign_in') // Acessa a página de login
+
+    // Preenche o campo de usuário
+    cy.get("[data-qa-selector='login_field']").type(user)
+
+    // Preenche o campo de senha (sem mostrar no log)
+    cy.get("[data-qa-selector='password_field']").type(password, { log: false })
+
+    // Clica no botão de login
+    cy.get("[data-qa-selector='sign_in_button']").click()
+  }
+
+  login() // Executa a função de login
+})
